6602719af3044d5e579e6e75cb82ee84e3288afd
Dimpal273 <dimpal.shah@student.csulb.edu>
2023-06-05 23:25:10 -0700

    branch push working

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="ButtonScreen" component={ButtonScreen} />
         <Stack.Screen name="ContactsScreen" component={ContactsScreen} />
-        {/* branch push not working*/}
+        {/* branch push working*/}
       </Stack.Navigator>
     </NavigationContainer>
   );
