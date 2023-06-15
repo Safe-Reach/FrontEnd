657a54d8e5491a2aa2f22e4510e631dd1f7570d6
NoMamesWey1 <josejimenez530@yahoo.com>
2023-06-14 23:27:25 -0700

    correct code

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,103 @@
-import React, { Component } from "react";
-import { Button, View, Text,TouchableOpacity, StyleSheet } from "react-native";
-export default function LoginScreen({navigation}) {
-  return (
-    <View style={styles.conatiner}>
-      <Text>Login Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("HomeScreen")}>
-      <Text>Go to HomeScreen</Text>
-      </TouchableOpacity>
+//Login.js
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
 
-      <TouchableOpacity onPress={() => navigation.navigate("SignUpScreen")}>
-      <Text>Go to SignUp Screen</Text>
+export default function Login({ navigation }) {
+  const LogoImage = require('../assets/logo.png');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const pressLogin = () => {
+    // Add code to authenticate login info once the database is set up
+    navigation.navigate('HomeScreen');
+  };
+
+  const pressSignup = () => {
+    navigation.navigate('SignUpScreen');
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Image source={LogoImage} style={styles.logo} />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        style={styles.textBoxes}
+        onChangeText={(text) => { setUsername(text) }}
+      />
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Password"
+        value={password}
+        style={styles.textBoxes}
+        onChangeText={(Text) => { setPassword(Text) }}
+      />
+      <TouchableOpacity onPress={pressLogin} style={styles.loginButton}>
+        <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-    </View>
+      <TouchableOpacity onPress={pressSignup} style={styles.signupButton}>
+        <Text style={styles.signupButtonText}>Signup</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
-const styles = StyleSheet.create({
-    conatiner: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginTop: 40,
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+  },
+  title: {
+    color: '#000000',
+    alignSelf: 'flex-start',
+    marginLeft: 35,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  textBoxes: {
+    width: '80%',
+    fontSize: 18,
+    textAlign: 'left',
+    padding: 12,
+    marginBottom: 20,
+    backgroundColor: '#f8f8f8',
+    borderRadius: 10,
+  },
+  loginButton: {
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: '#79e0d4',
+    paddingVertical: 14,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+  },
+  signupButton: {
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    paddingVertical: 14,
+    borderRadius: 10,
+  },
+  signupButtonText: {
+    color: '#b0a9a9',
+    fontSize: 18,
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+  },
 });
--- screens/SignUpSceen.js
+++ screens/SignUpSceen.js
@@ -1,9 +1,89 @@
-import React, { Component } from "react";
-import { Button, View, Text } from "react-native";
-export default function SignUpScreen() {
+// SignupPage.js
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function SignupPage({ navigation }) {
+
+  const pressSignup = () => { //need to add code for validate user input for all fields
+    navigation.navigate('LoginScreen');
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>SignUp Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.backgroundBox} />
+      <View style={styles.contentBox}>
+        <Text style={styles.title}>Create Account</Text>
+        <TextInput
+          placeholder="First Name"
+          style={styles.textInput}
+        />
+        <TextInput
+          placeholder="Last Name"
+          style={styles.textInput}
+        />
+        <TextInput
+          placeholder="Email Name"
+          style={styles.textInput}
+        />
+        <TextInput
+          placeholder="Phone Number"
+          style={styles.textInput}
+        />
+        <TextInput
+          placeholder="Birth date"
+          style={styles.textInput}
+        />
+        <TouchableOpacity onPress={pressSignup} style={styles.signupButton}>
+          <Text style={styles.signupButtonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundBox: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#c6f5eb',
+  },
+  contentBox: {
+    width: '80%',
+    backgroundColor: 'black',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  textInput: {
+    width: '100%',
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  signupButton: {
+    backgroundColor: '#79e0d4',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  signupButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
